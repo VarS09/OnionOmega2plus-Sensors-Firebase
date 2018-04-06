f6236f93a379f6eb907709266c2e4671961d60c5
Sushant Varada <sushantvarada@gmail.com>
2018-04-06 14:13:32 +0530

    remove FirePush.py

--- FirePush.py
+++
@@ -1,7 +0,0 @@
-from firebase import firebase
-
-firebase=firebase.FirebaseApplication('https://pushpulltest.firebaseio.com/')
-
-resultPut=firebase.put('pushpulltest','five','0')
-
-print(resultPut)
