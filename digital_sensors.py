b3d49a252427c95c7cd6e2fed9282ef1fb978be3
loser2509 <34755328+loser2509@users.noreply.github.com>
2018-05-08 16:17:19 +0530

    Update digital_sensors.py

--- digital_sensors.py
+++ digital_sensors.py
@@ -15,43 +15,32 @@ gpio0=onionGpio.OnionGpio(0)
 status=gpio0.setInputDirection()
 initialvalue=gpio0.getValue()
 
-#Initializing Hazardous gas detector sensor
-gpio1=onionGpio.OnionGpio(1)
-status=gpio1.setInputDirection()
-initialvalue1=gpio1.getValue()
-
 #Loop to sense data indefinitely
 while(1):
 
 	try:
 		#Fetching DHT sensor data in json format
-        info=subprocess.check_output("dht-sensor 19 DHT11 json",shell=True)
+        	info=subprocess.check_output("dht-sensor 19 DHT11 json",shell=True) #Connected DHT11 to gpio 19
 		data=json.loads(info)
-        #Just to verify
-        print('Sensor data')
+       		#Just to verify
+        	print('Sensor data')
 		print(info)
-        #Pushing DHT sensor data to firebase
+        	#Pushing DHT sensor data to firebase
 		result1=firebase.put('/sensor data','temperature',data['temperature'])
 		result2=firebase.put('/sensor data','humidity',data['humidity'])
+		print(result1) #Just to verify
+		print(result2) #Just to verify
 		
-        #Fetching Flame sensor data and pushing it to firebase
-        value=gpio0.getValue()
-        if value!=initialvalue:
+        	#Fetching Flame sensor data and pushing it to firebase
+        	value=gpio0.getValue()
+        	if value!=initialvalue:
 			print('Flame ON!!') #Just to verify
 			result3=firebase.put('/sensor data','checkfire','1')
 		else:
 			print('Flame OFF??') #Just to verify
 			result3=firebase.put('/sensor data','checkfire','-1')
 		
-        #Fetching Hazardous gas detector sensor data and pushing it to firebase
-        value1=gpio1.getValue()
-        if value1!=initialvalue1:
-			print('Hazardous gas detected!!') #Just to verify
-			result4=firebase.put('/sensor data','checkair','1')
-		else:
-			print('Air Quality OK!!') #Just to verify
-			result4=firebase.put('/sensor data','checkair','-1')
-		
+		print(result3) #Just to verify
 		time.sleep(5)
 		
 	except KeyboardInterrupt:
